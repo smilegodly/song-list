5963b2783a8b9b943ace46164d5240e33ec377cc
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2021-03-26 14:35:55 -0400

    Added react-redux into the application Added a songs and selectedSong reducer for the store Added the SongList component Added an action creator

---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    {
+      title: "Take On Me",
+      duration: "4:03",
+    },
+    {
+      title: "Never Gonna Give You Up",
+      duration: "3:33",
+    },
+    {
+      title: "All Star",
+      duration: "3:15",
+    },
+    {
+      title: "Lithium Flower",
+      duration: "3:22",
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
